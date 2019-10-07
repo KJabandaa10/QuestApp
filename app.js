9f2d2cb16bf80e553f9d455a232ff0b5cb4bd612
Kartar Singh Jabanda <kartarjabanda@hotmail.co.uk>
2019-10-07 19:37:38 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// vars - change with random function according to users boundaries
+let a = Math.floor(Math.random() * 100);
+let b = 17;
+let c = 2;
+let d = 0.433;
+let e = 550;
+let f = 19221;
+let g = -14.8;
+
+const x = a + 2 * c - e + f + 1000 * g;
+console.log(x);
+
+// random number between specified params
+function randInt() {
+  let min = 1; // user input determines min  max values // input.value => see weather app code
+  let max = 10;
+  const randomnum = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(randomnum);
+}
+
+function randDecimal() {
+  let decimal = 100; // user input determines decimal points // input.value => see weather app code
+  let integral = 10;
+  const randomnum =
+    Math.floor(
+      Math.random() * (integral * decimal - 1 * decimal) + 1 * decimal
+    ) /
+    (1 * decimal);
+  console.log(randomnum);
+}
+
+// add event listeners to call functions when params have been accepted
